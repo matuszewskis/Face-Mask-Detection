bc9741d4dc9f0f179adea4585d50cc5e90432b26
unknown <mateusz.kierznowski99@gmail.com>
2021-10-26 08:48:19 +0200

    initial commit

---
+++ ConV.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, Input, ZeroPadding2D, BatchNormalization, Activation, MaxPooling2D, Flatten, Dense, \
+    Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+
+from keras.models import Model, load_model
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.utils import shuffle
+import numpy as np
+
+# model 2
+model = Sequential([
+    Conv2D(100, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    MaxPooling2D(2, 2),
+
+    Conv2D(100, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+
+    Flatten(),
+    Dropout(0.5),
+    Dense(50, activation='relu'),
+    Dense(2, activation='softmax')
+])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
+
+path = os.getcwd()
+path = os.path.join(path, 'dataset')
+
+# declare train_path
+path_train = os.path.join(path, 'Train')
+
+# declare test_path
+path_test = os.path.join(path, 'Test')
+
+train_datagen = ImageDataGenerator(rescale=1.0 / 255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+train_generator = train_datagen.flow_from_directory(path_train,
+                                                    batch_size=10,
+                                                    target_size=(150, 150))
+
+validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
+
+validation_generator = validation_datagen.flow_from_directory(path_test,
+                                                              batch_size=10,
+                                                              target_size=(150, 150))
+
+history = model.fit_generator(train_generator,
+                              epochs=3,
+                              validation_data=validation_generator,
+                              )
+
+img = next(train_generator)[0][0].reshape(1, 150, 150, 3)
+model.predict(img)
+model.predict(next(train_generator)[0])
+
+model.predict(next(train_generator)[0])
